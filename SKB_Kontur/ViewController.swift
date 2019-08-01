db7c3e4a93971ce0397fa1ccc5dfbf7d54d4e322
Mikhail <gagin@mysmartflat.ru>
2019-08-01 08:38:47 +0500

    Initial Commit

---
+++ SKB_Kontur/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SKB_Kontur
+//
+//  Created by Admin on 01/08/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
